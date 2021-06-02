937198f2a76da5432ca929f11c6cb53ca0f89b98
Viking Falk Pettersson <vikingfalk@gmail.com>
2021-06-02 16:22:39 +0200

    about page taking form

--- components/About.js
+++ components/About.js
@@ -5,6 +5,27 @@ const About = () => {
 		<section className={aboutStyles.about}>
 			<div className="container">
 				<h2 className={aboutStyles.about__heading}>About Me</h2>
+				<div className={aboutStyles.grid}>
+					<article className={aboutStyles.card + " " + aboutStyles.bg}>
+						<h3 className={aboutStyles.card__heading}>my-background:</h3>
+						<p>
+							My background is very awesome in a lot of ways. My background is very awesome in a lot
+							of ways. My background is very awesome in a lot of ways. My background is very awesome
+							in a lot of ways.
+						</p>
+					</article>
+					<article className={aboutStyles.card + " " + aboutStyles.exp}>
+						<h3 className={aboutStyles.card__heading}>my-experience:</h3>
+						<p>My background is very awesome in a lot of ways.</p>
+					</article>
+					<article className={aboutStyles.card + " " + aboutStyles.int}>
+						<h3 className={aboutStyles.card__heading}>my-interests:</h3>
+						<p>
+							My background is very awesome in a lot of ways. My background is very awesome in a lot
+							of ways. My background is very awesome in a lot of ways.
+						</p>
+					</article>
+				</div>
 			</div>
 		</section>
 	);
